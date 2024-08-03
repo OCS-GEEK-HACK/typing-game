c50db66829447367f501bed11887792aa128cffd
yuto ichinose <yuto.0526.sano@gmail.com>
2024-08-03 16:12:11 +0900

    Revert "feat: 画面切り替えロジックと各画面のモック作成"
    This reverts commit 2e66efebd6b266198479981b386bf78438e2c959.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,24 +1,5 @@
 "use strict";
 
-import { GameScreen } from "./modules/screens/game-screen.js";
-import { ScreenManager } from "./modules/screen-maneger.js";
-import { SelectModeScreen } from "./modules/screens/select-mode-screen.js";
-import { SettingScreen } from "./modules/screens/setting-screen.js";
-import { TitleScreen } from "./modules/screens/title-screen.js";
+import { Hello } from "./modules/hello.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const screenManager = new ScreenManager();
-  screenManager.showScreen("title");
-
-  const titleScreen = new TitleScreen(screenManager);
-  titleScreen.initialize();
-
-  const settingScreen = new SettingScreen(screenManager);
-  settingScreen.initialize();
-
-  const selectModeScreen = new SelectModeScreen(screenManager);
-  selectModeScreen.initialize();
-
-  const gameScreen = new GameScreen(screenManager);
-  gameScreen.initialize();
-});
+Hello();
