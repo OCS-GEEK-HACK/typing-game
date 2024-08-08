25ac8144ab92ca4f08477f3986f713645ba97407
Tabigon74 <zerows.y@gmail.com>
2024-08-08 23:11:12 +0900

    fix: コンストラクタを使う形に変更,書き換え後のレイアウト変更

--- public/javascripts/modules/screens/game-screen.js
+++ public/javascripts/modules/screens/game-screen.js
@@ -12,6 +12,24 @@ export class GameScreen {
   constructor(screenManager) {
     /** @type {ScreenManager} */
     this.screenManager = screenManager;
+    this.Q = [
+      "ディレクトリトラバーサル",
+      "基本情報技術者試験",
+      "クロスサイトリクエストフォージェリ",
+      "アイリス認証",
+    ]; //問題文
+    this.Q_roman = [
+      "delirekutoritoraba-saru",
+      "kihonjouhougijutushashiken",
+      "kurosusaitorikuesutofo-jeri",
+      "airisuninshou",
+    ]; //問題文
+    this.Q_No = Math.floor(Math.random() * this.Q_roman.length); //問題をランダムで出題する
+    this.Q_i = 0; //回答初期値・現在単語どこまで合っているか判定している文字番号
+    this.Q_l = this.Q[this.Q_No].length; //計算用の文字の長さ
+    this.Q_roman_i = 0; //回答初期値・現在単語どこまで合っているか判定している文字番号
+    this.Q_roman_l = this.Q_roman[this.Q_No].length; //計算用の文字の長さ
+    //関数内から直接触れない!
   }
 
   /**
@@ -26,27 +44,16 @@ export class GameScreen {
    * Starts the game.
    */
   startGame() {
-    this.screenManager.showScreen("game");
     // ゲームのロジックをここに追加
-
-    let Q = [
-      "ディレクトリトラバーサル",
-      "基本情報技術者試験",
-      "クロスサイトリクエストフォージェリ",
-      "アイリス認証",
-    ]; //問題文
-    let Q_roman = [
-      "delirekutoritoraba-saru",
-      "kihonjouhougijutushashiken",
-      "kurosusaitorikuesutofo-jeri",
-      "airisuninshou",
-    ]; //問題文
-    let Q_No = Math.floor(Math.random() * Q_roman.length); //問題をランダムで出題する
-
-    let Q_i = 0; //回答初期値・現在単語どこまで合っているか判定している文字番号
-    let Q_l = Q[Q_No].length; //計算用の文字の長さ
-    let Q_roman_i = 0; //回答初期値・現在単語どこまで合っているか判定している文字番号
-    let Q_roman_l = Q_roman[Q_No].length; //計算用の文字の長さ
+    let Q = this.Q;
+    let Q_roman = this.Q_roman; //問題文
+    let Q_No = this.Q_No; //問題をランダムで出題する
+    let Q_i = this.Q_i; //回答初期値・現在単語どこまで合っているか判定している文字番号
+    let Q_l = this.Q_l; //計算用の文字の長さ
+    let Q_roman_i = this.Q_roman_i; //回答初期値・現在単語どこまで合っているか判定している文字番号
+    let Q_roman_l = this.Q_roman_l; //計算用の文字の長さ
+    let count = 0;
+    console.log(this.screenManager.score + "score"); //screenManagerのscoreデータ
 
     window.addEventListener("keydown", push_Keydown);
 
@@ -66,12 +73,23 @@ export class GameScreen {
         //押したキーが合っていたら
 
         Q_roman_i++; //判定する文章に１足す
-        document.getElementById("word_roman").innerHTML =
-          '<span class="red">' +
-          Q_roman[Q_No].substring(0, Q_roman_i) +
-          '</span><span class="">' +
-          Q_roman[Q_No].substring(Q_roman_i, Q_roman_l) +
-          "</span>"; //問題を書き出す
+        let HTML = "";
+        console.log(HTML);
+        for (let i = 0; i < Q_roman_i; i++) {
+          HTML =
+            HTML +
+            '<span class="red">' +
+            Q_roman[Q_No].substring(i, i + 1) +
+            "</span>";
+        }
+        for (let i = Q_roman_i; i < Q_roman_l; i++) {
+          HTML =
+            HTML +
+            '<span class="">' +
+            Q_roman[Q_No].substring(i, i + 1) +
+            "</span>";
+        }
+        document.getElementById("word_roman").innerHTML = HTML;
 
         if (Q_roman_l - Q_roman_i === 0) {
           //全部正解したら
@@ -92,7 +110,15 @@ export class GameScreen {
             '<span class="">' +
             Q_roman[Q_No].substring(Q_roman_i, Q_roman_l) +
             "</span>"; //新たな問題を書き出す
+
+          count++; //正答数加算
+          console.log(count + "count");
         }
+      }
+
+      if (count > 2) {
+        console.log("Goresult");
+        this.showResults(); //呼び出せない!?
       }
     }
   }
