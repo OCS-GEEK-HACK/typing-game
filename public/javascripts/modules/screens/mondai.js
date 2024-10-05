216de7f1e7a414cdfeaf75d93408f597e5c4b0c1
otoha5 <s2370217@s.do-johodai.com>
2024-10-06 00:58:15 +0900

    feat: 問題の追加

--- public/javascripts/modules/screens/mondai.js
+++ public/javascripts/modules/screens/mondai.js
@@ -2,42 +2,242 @@ export const data = {
   low: {
     normal: [
       {
-        word: "en",
-        key: "en",
+        word: "宝石",
+        key: "houseki",
+      },
+      {
+        word: "キジムナー",
+        key: "kijimuna-",
+      },
+      {
+        word: "ずんだもち",
+        key: "zunndamoti",
+      },
+      {
+        word: "ウォーターサーバー",
+        key: "who-ta-sa-ba-",
+      },
+      {
+        word: "アレンジ",
+        key: "arenji",
+      },
+      {
+        word: "ティッシュペーパー",
+        key: "thissyupe-pa-",
+      },
+      {
+        word: "羊",
+        key: "hituji",
+      },
+      {
+        word: "アンドロメダ座",
+        key: "anndoromedaza",
+      },
+      {
+        word: "老若男女",
+        key: "rounyakunannnyo",
+      },
+      {
+        word: "生卵",
+        key: "namatamago",
+      },
+      {
+        word: "炙りカルビ",
+        key: "aburikarubi",
+      },
+      {
+        word: "三塁打",
+        key: "sannruida",
+      },
+      {
+        word: "骨粗鬆症",
+        key: "kotusosyousyou",
+      },
+      {
+        word: "バスガス爆発",
+        key: "basugasubakuhatu",
+      },
+      {
+        word: "さくらんぼ",
+        key: "sakurannbo",
+      },
+      {
+        word: "新設診察室視察",
+        key: "sinnsetusinnsatusitusisatu",
+      },
+      {
+        word: "貨客船",
+        key: "kakyakusenn",
+      },
+      {
+        word: "火星探査車",
+        key: "kaseitannsasya",
+      },
+      {
+        word: "春日部",
+        key: "kasukabe",
       },
     ],
     it: [
       {
-        word: "ei",
-        key: "ei",
+        word: "ソフトウェア開発",
+        key: "sofutoweakaihatu",
+      },
+      {
+        word: "ハードウェア",
+        key: "ha-doweakaihatu",
+      },
+      {
+        word: "バグ",
+        key: "bagu",
+      },
+      {
+        word: "ブラウザ",
+        key: "burauza",
+      },
+      {
+        word: "クラウド",
+        key: "kuraudo",
+      },
+      {
+        word: "ビッグデータ",
+        key: "biggude-ta",
+      },
+      {
+        word: "IPアドレス",
+        key: "ipadoresu",
+      },
+      {
+        word: "キャッシュ",
+        key: "kyassyu",
+      },
+      {
+        word: "関数",
+        key: "kannsuu",
+      },
+      {
+        word: "デバック",
+        key: "debakku",
+      },
+      {
+        word: "セキュリティー",
+        key: "sekyurithi-",
       },
     ],
   },
   middle: {
     normal: [
       {
-        word: "nn",
-        key: "nn",
+        word: "耳を洗う",
+        key: "mimiwoarau",
+      },
+      {
+        word: "アーチ式ダム",
+        key: "a-tisikidamu",
+      },
+      {
+        word: "アームストロング",
+        key: "a-musutoronngu",
+      },
+      {
+        word: "絵空事",
+        key: "esoragoto",
+      },
+      {
+        word: "イマジネーション",
+        key: "imajine-syonn",
       },
     ],
     it: [
       {
-        word: "ni",
-        key: "ni",
+        word: "インターフェイス",
+        key: "innta-feisu",
+      },
+      {
+        word: "オフショア",
+        key: "ohusyoa",
+      },
+      {
+        word: "スクレイピング",
+        key: "sukureipinngu",
+      },
+      {
+        word: "データマイニング",
+        key: "de-tamaininngu",
+      },
+      {
+        word: "仮想化",
+        key: "kasouka",
+      },
+      {
+        word: "機械学習",
+        key: "kikaigakusyuu",
+      },
+      {
+        word: "オブザーバビリティ",
+        key: "obuza-babirithi",
+      },
+      {
+        word: "オルタナティブデータ",
+        key: "orutanathibude-ta",
       },
     ],
   },
   high: {
     normal: [
       {
-        word: "hn",
-        key: "hn",
+        word: "生麦生米生卵",
+        key: "namamuginamagomenamatamago",
+      },
+      {
+        word: "交響曲歌曲協奏曲",
+        key: "koukyoukyokukakyokukyousoukyoku",
+      },
+      {
+        word: "国語熟語述語主語",
+        key: "kokugojyukugojyukugosyugo",
+      },
+      {
+        word: "輸出車輸出湯輸出酢",
+        key: "yusyutusyayusyutuyuyusyutusu",
+      },
+      {
+        word: "東京特許許可局",
+        key: "toukyoutokkyokyokakyoku",
       },
     ],
     it: [
       {
-        word: "hi",
-        key: "hi",
+        word: "ディジタルトランスフォーメーション",
+        key: "delijitarutoransufo-me-shon",
+      },
+      {
+        word: "ジオメディア",
+        key: "jiomedhia",
+      },
+      {
+        word: "デジタル免疫システム",
+        key: "dejitalmennekisisutemu",
+      },
+      {
+        word: "ハイパーオートメーション",
+        key: "haipa-o-tome-syonn",
+      },
+      {
+        word: "プライバシー強化コンピュテーション",
+        key: "puraibasi-kyoukakonnpyute-syonn",
+      },
+      {
+        word: "ディジタルトランスフォーメーション",
+        key: "delijitarutoransufo-me-shon",
+      },
+      {
+        word: "プロンプトエンジニアリング",
+        key: "puronnputoennjiniarinngu",
+      },
+      {
+        word: "ナレッジマネジメント",
+        key: "nerejjimanejimennto",
       },
     ],
   },
